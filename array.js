575efa784b30c8f2c6395caa9f352d13a444f830
CARLOS DANIEL FREITAS LIMA LINS <carloslins6232@digitalcollege.internal>
2025-06-10 20:19:35 -0300

    Ajustando

---
+++ array.js
@@ -0,0 +1,18 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let nomes = ["João", "Maria", "José", "Pedro", "Ana", "Carlos", "Mariana", "Paulo", "Sandra", "Ricardo", "Fernanda"];       
+
+// // console.log(`Tamanho do array numeros: ${numeros.length}`);
+
+// nomes.push("Roberta");
+// console.log(nomes);
+ 
+let nomeProprio = `${nomes[6]} ${nomes[7]}`
+// console.log(nomeProprio.toUpperCase())
+
+
+let frutas = ["maçã", "banana", "laranja", "uva", "abacaxi"];
+
+frutas.forEach(function(fruta) {
+  console.log(fruta);
+});
